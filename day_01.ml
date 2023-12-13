a16d44d1c8dc82ce30f390f104a4f1155d1a5e58
Maciek Makowski <maciek@mmakowski.com>
2023-12-13 19:49:34 +0100

    day 1 part 2 done

--- day_01.ml
+++ day_01.ml
@@ -4,12 +4,27 @@
 #use "day_01.ml";;
 *)
 
+let digit search_fn str =
+  let digit_regexp = Str.regexp {|[0-9]\|zero\|one\|two\|three\|four\|five\|six\|seven\|eight\|nine|} in
+  search_fn digit_regexp str;
+  match Str.matched_string str with
+    | "zero" -> "0"
+    | "one" -> "1"
+    | "two" -> "2"
+    | "three" -> "3"
+    | "four" -> "4"
+    | "five" -> "5"
+    | "six" -> "6"
+    | "seven" -> "7"
+    | "eight" -> "8"
+    | "nine" -> "9"
+    | x when String.length x = 1 -> x
+    | other -> raise (Invalid_argument other)
+
 let line_value line =
-  let digit_regexp = Str.regexp "[0-9]" in
-  let first_digit_pos = Str.search_forward digit_regexp line 0 in
-  let last_digit_pos = Str.search_backward digit_regexp line (String.length line) in
-  let digit pos = String.sub line pos 1 in
-  let num_str = String.cat (digit first_digit_pos) (digit last_digit_pos) in
+  let first_digit = digit (fun regexp str -> Str.search_forward regexp str 0) line in
+  let last_digit = digit (fun regexp str -> Str.search_backward regexp str (String.length str)) line in
+  let num_str = String.cat first_digit last_digit in
   let num = int_of_string num_str in
   (* Printf.printf "%d\n" num; *)
   num
