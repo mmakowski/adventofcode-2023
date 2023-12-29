ce558fc1833a2925c946e9951c1be819c81d8600
Maciek Makowski <maciek@mmakowski.com>
2023-12-29 11:26:13 +0100

    day 8 part 2 done

--- day_08.ml
+++ day_08.ml
@@ -1,4 +1,4 @@
-(* #require "str";; *)
+#require "str";;
 
 type fork = {
   left: string;
@@ -44,7 +44,7 @@ let parse_input f =
 let rec walk all_dirs graph step_no loc = function
   | [] -> walk all_dirs graph step_no loc all_dirs;
   | dir::dirs -> 
-    if loc = "ZZZ" then
+    if String.ends_with ~suffix:"Z" loc then
       step_no
     else
       let {left; right} = Graph.find loc graph in
@@ -55,10 +55,26 @@ let rec walk all_dirs graph step_no loc = function
     in walk all_dirs graph (step_no+1) next_loc dirs
 ;;
 
+let rec gcd a b =
+  if b = 0 then a else gcd b (a mod b)
+;;
+
+let lcm a b =
+  a * b / (gcd a b)
+;;
+
+let is_start_loc (loc, _) =
+  if String.ends_with ~suffix:"A" loc then
+    Some loc
+  else None
+;;
+
 let result () = 
   let f = open_in "input-08.txt" in
   let dirs, graph = parse_input f in
-  walk dirs graph 0 "AAA" dirs
+  let start_locs = List.filter_map is_start_loc (Graph.bindings graph) in
+  let step_nums = List.map (fun loc -> walk dirs graph 0 loc dirs) start_locs in
+  List.fold_left lcm 1 step_nums
 ;;
 
 Printf.printf "%d\n" (result ());;
